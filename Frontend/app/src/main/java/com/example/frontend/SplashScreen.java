d13fb1f4eeea36605a5157b2a5a4eb0a0ac10a60
PularaPiyumali <120791863+PularaPiyumali@users.noreply.github.com>
2023-03-27 08:12:04 +0530

    Update SplashScreen.java

--- Frontend/app/src/main/java/com/example/frontend/SplashScreen.java
+++ Frontend/app/src/main/java/com/example/frontend/SplashScreen.java
@@ -17,5 +17,15 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+
+
+            }
+        },3000);
+
     }
 }
