017b9f3352a14ad3d27354ff5f13b783cc477c74
PularaPiyumali <120791863+PularaPiyumali@users.noreply.github.com>
2023-03-27 07:11:31 +0530

    Creating SplashScreen class

---
+++ Frontend/app/src/main/java/com/example/frontend/SplashScreen.java
@@ -0,0 +1,11 @@
+package com.example.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+}
