4a15917ede56e68c68c3916a3d3316ec9c10fb11
Hasandi Lavanya <hl.gunasekera2001@gmail.com>
2023-03-29 19:32:42 +0530

    Setting the start button in Main activity

--- Frontend/app/src/main/java/com/example/frontend/MainActivity.java
+++ Frontend/app/src/main/java/com/example/frontend/MainActivity.java
@@ -16,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.start);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,selectionActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
