686f97c31dde03a13473613923ec6eee40ad3c74
PularaPiyumali <120791863+PularaPiyumali@users.noreply.github.com>
2023-03-30 19:17:47 +0530

    Adding variables to ProgressBarAnimation.java

--- Frontend/app/src/main/java/com/example/frontend/ProgressBarAnimation.java
+++ Frontend/app/src/main/java/com/example/frontend/ProgressBarAnimation.java
@@ -1,4 +1,17 @@
 package com.example.frontend;
 
-public class ProgressBarAnimation {
+import android.content.Intent;
+import android.view.animation.Animation;
+import android.content.Context;
+import android.view.animation.Transformation;
+import android.widget.TextView;
+import android.widget.ProgressBar;
+
+public class ProgressBarAnimation extends Animation {
+
+    private Context con;
+    private ProgressBar progressionBar;
+    private TextView text;
+    private float from;
+    private float to; 
 }
