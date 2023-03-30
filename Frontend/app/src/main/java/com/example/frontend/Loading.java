9452e7d4dd907c0154e0cc0cbb2c11502bccdd21
Hasandi Lavanya <hl.gunasekera2001@gmail.com>
2023-03-30 10:36:17 +0530

    import libraries in loading page

---
+++ Frontend/app/src/main/java/com/example/frontend/Loading.java
@@ -0,0 +1,24 @@
+package com.example.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
