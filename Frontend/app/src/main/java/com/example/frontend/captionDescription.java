c28e74d8b9954ba5ee3aeec37c4e5e875e6fc5f0
Hasandi Lavanya <hl.gunasekera2001@gmail.com>
2023-04-03 21:27:48 +0530

    caption description edit

---
+++ Frontend/app/src/main/java/com/example/frontend/captionDescription.java
@@ -0,0 +1,29 @@
+package com.example.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;import android.os.Bundle;
+
+public class captionDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caption_description);
+    }
+}
