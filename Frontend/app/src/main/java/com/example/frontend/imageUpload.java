7c30eb6a8e637619171c01df7136ec3b30b851bc
Hasandi Lavanya <hl.gunasekera2001@gmail.com>
2023-04-03 21:37:21 +0530

    updated image upload page

---
+++ Frontend/app/src/main/java/com/example/frontend/imageUpload.java
@@ -0,0 +1,25 @@
+package com.example.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class imageUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_upload);
+    }
+}
