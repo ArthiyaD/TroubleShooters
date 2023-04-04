b434cffb74a409d2807011c1925643834f225596
PularaPiyumali <120791863+PularaPiyumali@users.noreply.github.com>
2023-04-04 13:53:51 +0530

    Import libraries to videoUpload.java

--- Frontend/app/src/main/java/com/example/frontend/videoUpload.java
+++ Frontend/app/src/main/java/com/example/frontend/videoUpload.java
@@ -1,4 +1,26 @@
 package com.example.frontend;
 
+import static android.content.ContentValues.TAG;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import android.media.MediaPlayer.OnPreparedListener;
 public class videoUpload {
 }
