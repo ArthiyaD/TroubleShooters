bcc0fc5ba6772d83865f617c30060110f4c71aee
Hasandi Lavanya <hl.gunasekera2001@gmail.com>
2023-04-04 09:26:33 +0530

    updated the caption activity

--- Frontend/app/src/main/java/com/example/frontend/caption.java
+++ Frontend/app/src/main/java/com/example/frontend/caption.java
@@ -2,7 +2,20 @@ package com.example.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class caption extends AppCompatActivity {
 
