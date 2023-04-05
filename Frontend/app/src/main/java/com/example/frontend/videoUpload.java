d24fa87bb2e7f9d1fb91eb613cc0eac87c0d4a55
Hasandi Lavanya <hl.gunasekera2001@gmail.com>
2023-04-06 00:33:40 +0530

    made videoupload page

---
+++ Frontend/app/src/main/java/com/example/frontend/videoUpload.java
@@ -0,0 +1,32 @@
+package com.example.frontend;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+public class videoUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_upload);
+    }
+}
